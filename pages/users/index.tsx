76fcdb3e774056f4569117e80322f44861949c9f
Tomáš Witek <tomas@witek.cz>
2020-09-28 19:25:32 +0200

    FaunaDB

---
+++ pages/users/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {}
+
+function UsersPage(props: Props) {
+  const {} = props;
+
+  return "UsersPage";
+}
+
+export default UsersPage;
---
+++ pages/users/new.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {}
+
+function NewUserPage(props: Props) {
+  const {} = props;
+
+  return "NewUserPage";
+}
+
+export default NewUserPage;
